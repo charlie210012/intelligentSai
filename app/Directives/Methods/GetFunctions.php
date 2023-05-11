07d4354b51e9a712b77bbabd936204fb74b68a49
caarevalo <caarevalo@cognox.com>
2023-05-10 20:34:55 -0500

    feat: Se añade la funcionalidad a la lista de funcionalidades terminadas

--- app/Directives/Methods/GetFunctions.php
+++ app/Directives/Methods/GetFunctions.php
@@ -11,7 +11,8 @@ class GetFunctions implements DirectivesAnswers
         return [
             "message" =>[
             "1. puedes preguntarme por tus datos, tu nombre, tu identificación, tu email, o tu fecha de creación - estado:Terminada",
-            "2. Consultar datos de los empleados - estado: Terminados",
+            "2. Consultar datos de los empleados - estado: Terminada",
+            "3. Registrar nuevos empleados - estado: Terminada"
             ]
         ];
     }
